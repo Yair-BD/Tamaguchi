77faabfa9e26dadb5db2666e044e92ad169d35ca
Yair <yair2321@gmail.com>
2021-06-30 10:53:48 +0300

    update names and func

--- clear_1.py
+++ clear_1.py
@@ -1,11 +1,4 @@
 def Clear():
+    for n in range(2):
+        print()
 
-    def clear_2():
-        for n in range(2):
-            print()
-
-    clear_2()
-
-
-if __name__ == '__main__':
-    Clear()
--- create_1.py
+++ create_1.py
@@ -1,19 +1,5 @@
-from Tamaguchi.Animal import Dog, Turtle
+from Tamaguchi import *
 
 
-def Create():
-    def create(danimal={}):
-        new_animal = input("Dog or Turtle ??\n").lower()
-        new_name = input("What his name ??\n").lower()
-        new_color = input("Which color ??\n").lower()
-        if new_animal == "dog":
-            danimal = Dog(new_name.capitalize(), new_color.capitalize())
-        elif new_animal == "turtle":
-            new_name = Turtle(new_name.capitalize(), new_color.capitalize())
-        return f"{new_name} created successfully"
-
-    create()
 
 
-if __name__ == '__main__':
-    Create()
